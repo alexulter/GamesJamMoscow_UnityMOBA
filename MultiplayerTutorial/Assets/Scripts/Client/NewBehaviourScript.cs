0efed1156ac846d660e3e4e1ae1e6ac749da2dae
Alex Zhukov <alexulter@gmail.com>
2013-12-07 17:03:18 +0400

    fix

--- MultiplayerTutorial/Assets/Scripts/Client/NewBehaviourScript.cs
+++ MultiplayerTutorial/Assets/Scripts/Client/NewBehaviourScript.cs
@@ -1,15 +1,3 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-}
