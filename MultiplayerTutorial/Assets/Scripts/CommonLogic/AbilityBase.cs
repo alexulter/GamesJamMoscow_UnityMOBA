19e4f5732055dbb7a0f65dd60640c9ff35048c03
Alex Zhukov <alexulter@gmail.com>
2013-12-08 14:59:35 +0400

    ability, UI

--- MultiplayerTutorial/Assets/Scripts/CommonLogic/AbilityBase.cs
+++ MultiplayerTutorial/Assets/Scripts/CommonLogic/AbilityBase.cs
@@ -4,18 +4,14 @@ using AssemblyCSharp;
 
 public class AbilityBase : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 	AimType AimType;
 	CastType CastType;
 	private double Manacost; //сколько маны нужно потратить на применение
 	private double Cooldown; //время отката абилки, в секундах
+	public string Name; //название абилки
+
+	public void OnCast ()
+	{
+	}
 
 }
