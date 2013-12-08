b7203f887107aed58a940288f0cea9f0ee6a4592
Alex Zhukov <alexulter@gmail.com>
2013-12-08 17:38:20 +0400

    added camera zoom by mousewheel

--- MultiplayerTutorial/Assets/Scripts/Client/CameraMove/CameraMoveRTS.cs
+++ MultiplayerTutorial/Assets/Scripts/Client/CameraMove/CameraMoveRTS.cs
@@ -23,6 +23,19 @@ public class CameraMoveRTS : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		//Зум по колёсику мыши
+
+		if (Input.GetAxis("Mouse ScrollWheel") < 0) // back
+		{
+			transform.Translate(0,0,-1);
+			
+		}
+		if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
+		{
+			transform.Translate(0,0,1);
+		}
+
+
 		//Идет проверка для нижнего края экрана	
 		if (Input.mousePosition.y < 20 && Input.mousePosition.y<=prevY){
 			y = moveSpeed * Input.GetAxis ("Mouse Y");
@@ -53,6 +66,10 @@ public class CameraMoveRTS : MonoBehaviour {
 
 		//Устанавливается новое положение камеры, с условием, что ось Х камеры парралельна оси Х области, и наклон сделан по оси Х
 		transform.Translate(x,y,y);
+
+
+
+
 	}
 	
 	
